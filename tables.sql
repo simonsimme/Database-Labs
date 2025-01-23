ac9167a2fc32582375d9a85d292940ca2bb26f20
Simon <64027803+simonsimme@users.noreply.github.com>
2025-01-23 15:37:24 +0100

    da

--- tables.sql
+++ tables.sql
@@ -1,1 +1,21 @@
 -- This file will contain all your tables
+-- This file will contain all your tables
+CREATE TABLE Students(
+    idnr TEXT PRIMARY KEY CHECK (idnr LIKE '__________'),
+    name TEXT,
+    login TEXT,
+    program TEXT
+);
+
+CREATE TABLE Branches(
+    name TEXT,
+    program TEXT
+);
+
+Courses(
+    code TEXT PRIMARY KEY CHECK (code LIKE '______'),
+    name TEXT,
+    credits REAL,
+    department TEXT
+);
+
