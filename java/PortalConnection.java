a197668459b794315a8c4d6115ff514b7675ac21
Jonathan Alm <144455369+jonathan63-63@users.noreply.github.com>
2025-03-03 15:48:09 +0100

    un-/reg done

---
+++ java/PortalConnection.java
@@ -0,0 +1,89 @@
+
+import java.sql.*; // JDBC stuff.
+import java.util.Properties;
+
+public class PortalConnection {
+
+    // Set this to e.g. "portal" if you have created a database named portal
+    // Leave it blank to use the default database of your database user
+    static final String DBNAME = "AssignmentDB";
+    // For connecting to the portal database on your local machine
+    static final String DATABASE = "jdbc:postgresql://localhost/"+DBNAME;
+    static final String USERNAME = "postgres";
+    static final String PASSWORD = "postgres";
+
+    // This is the JDBC connection object you will be using in your methods.
+    private Connection conn;
+
+    public PortalConnection() throws SQLException, ClassNotFoundException {
+        this(DATABASE, USERNAME, PASSWORD);  
+    }
+
+    // Initializes the connection, no need to change anything here
+    public PortalConnection(String db, String user, String pwd) throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        Properties props = new Properties();
+        props.setProperty("user", user);
+        props.setProperty("password", pwd);
+        conn = DriverManager.getConnection(db, props);
+    }
+
+
+    // Register a student on a course, returns a tiny JSON document (as a String)
+    public String register(String student, String courseCode){
+      
+      String queryString = "INSERT INTO Registrations VALUES ('" + student + "', '" + courseCode + "')";
+      try(PreparedStatement insertQuery = conn.prepareStatement(queryString)) 
+      {
+          int rowsAffected = insertQuery.executeUpdate(); 
+          System.out.println(rowsAffected + " row(s) affected.");
+          return "{\"success\":true\"}";
+      } catch (SQLException e) 
+      {
+          return "{\"success\":false, \"error\":\""+getError(e)+"\"}";
+      }      
+    }
+
+    // Unregister a student from a course, returns a tiny JSON document (as a String)
+    public String unregister(String student, String courseCode){
+      
+      String queryString = "DELETE FROM Registrations WHERE student='" + student + "' AND course='" + courseCode + "'";
+      try (PreparedStatement deleteQuery = conn.prepareStatement(queryString)) 
+      {
+          int rowsAffected = deleteQuery.executeUpdate();
+          if(rowsAffected == 0) return "Student is not in Registered/WaitingList";
+          return "{\"success\":true\"}";
+      } catch (SQLException e) 
+      {
+        return "{\"success\":false, \"error\":\""+getError(e)+"\"}";
+      }
+    }
+
+    // Return a JSON document containing lots of information about a student, it should validate against the schema found in information_schema.json
+    public String getInfo(String student) throws SQLException{
+        
+        try(PreparedStatement st = conn.prepareStatement(
+            // replace this with something more useful
+            "SELECT jsonb_build_object('student',idnr,'name',name,'program') AS jsondata FROM BasicInformation WHERE idnr=?"
+            );){
+            
+            st.setString(1, student);
+            
+            ResultSet rs = st.executeQuery();
+            
+            if(rs.next())
+              return rs.getString("jsondata");
+            else
+              return "{\"student\":\"does not exist :(\"}"; 
+        } 
+    }
+
+    // This is a hack to turn an SQLException into a JSON string error message. No need to change.
+    public static String getError(SQLException e){
+       String message = e.getMessage();
+       int ix = message.indexOf('\n');
+       if (ix > 0) message = message.substring(0, ix);
+       message = message.replace("\"","\\\"");
+       return message;
+    }
+}
